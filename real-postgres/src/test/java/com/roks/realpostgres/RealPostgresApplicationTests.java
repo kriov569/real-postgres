b535aa53ac3d320d957c3a8911821fc817663c39
kriov569 <nikita.veshnyakov2014@gmail.com>
2025-03-10 15:45:04 +0300

    Initial commit

---
+++ real-postgres/src/test/java/com/roks/realpostgres/RealPostgresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roks.realpostgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealPostgresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
